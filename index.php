d90ff4a1b6e265f652a05f929cf351fa895acc44
flyyy26 <rfchannel0@gmail.com>
2024-09-12 17:08:45 +0700

    Header

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php include 'template/header.php' ?>
+    
+    <div class="banner">
+        <div class="content_banner">
+            <span>Selamat datang di avi humanity</span>
+            <h1>Bersama, Kita Bisa Mengubah hidup mereka</h1>
+            <a href="#"><button>Baca Selengkapnya</button></a>
+        </div>
+    </div>
+
+<?php include 'template/footer.php' ?>
---
+++ template/header.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>AVI Humanity</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="shortcut icon" href="images/favicon.png" type="image/x-icon">
+</head>
+<body>
+    <div class="header">
+        <div class="logo">
+            <img src="images/logo.png" alt="Logo AVI | AVI Humanity">
+        </div>
+        <nav>
+            <ul>
+                <li><a href="#">Beranda</a></li>
+                <li><a href="#">Infaq</a></li>
+                <li><a href="#">Program</a></li>
+                <li><a href="#">Tentang Kami</a></li>
+                <li><a href="#">Kontak</a></li>
+            </ul>
+        </nav>
+    </div>
